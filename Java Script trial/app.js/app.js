e15da515a4b38e30af067b946c4c4a3f1db5ce4a
Eli <eliswerekio@gmail.com>
2022-06-29 16:42:52 +0200

    tried functions and console logging numbers

---
+++ Java Script trial/app.js/app.js
@@ -0,0 +1,17 @@
+function logNumbers(num1, num2) {
+  console.log(num1 + num2);
+  //what if I have return here, or how do I have return here, since I dont have any variables ?
+}
+logNumbers(1, 1);
+
+function multiplyNumbers(num3, num4) {
+  let result;
+  result = num3 * num4;
+  console.log(result);
+  //return num3 * num4;
+  return result;
+}
+multiplyNumbers(5, 5);
+
+//why DOES THIS NOT WORK ? T.T
+//how do I execute console log outside the function
