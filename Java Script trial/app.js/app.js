35c67b0f544f6377c06eb65dc8506a3fc9146b79
Eli <eliswerekio@gmail.com>
2022-06-29 19:11:21 +0200

    accessing values

--- Java Script trial/app.js/app.js
+++ Java Script trial/app.js/app.js
@@ -1,19 +1,55 @@
 function logNumbers(num1, num2) {
   console.log(num1 + num2);
-  //what if I have return here, or how do I have return here, since I dont have any variables ?
-  //What would I return ?
+  return num1 + num2;
 }
-logNumbers(1, 1);
+let tmp = logNumbers(1, 1);
+console.log(tmp);
 
 function multiplyNumbers(num3, num4) {
   let result;
   result = num3 * num4;
   console.log(result);
-  //return num3 * num4; =shorter version of this whole code ?
   return result;
 }
 let execution = multiplyNumbers(5, 5);
 console.log(execution);
 
-//why DOES THIS NOT WORK ? T.T
-//how do I execute console log outside the function
+function getPerson() {
+  let person = {
+    name: 'Georgi',
+    place: 'Amstetten',
+    age: 26,
+    taken: true,
+  };
+
+  let person2 = {
+    name: 'Eli',
+    place: 'Jarosov',
+    age: 19,
+    taken: true,
+  };
+
+  let array = [
+    person,
+    person2,
+    'Jirka',
+    [
+      'apples',
+      'banana',
+      'pear',
+      {
+        name: 'lemon',
+        color: 'yellow',
+      },
+    ],
+  ];
+  return array;
+}
+
+console.log(getPerson()[3][3].color);
+
+let georgiData = getPerson()[0];
+console.log(georgiData.name);
+
+let eliData = getPerson()[2];
+console.log(eliData);
