5149705e88fdf1b6fd21b888244b1656e0ab7508
Eli <eliswerekio@gmail.com>
2022-06-29 16:51:23 +0200

    updates

--- Java Script trial/app.js/app.js
+++ Java Script trial/app.js/app.js
@@ -1,6 +1,7 @@
 function logNumbers(num1, num2) {
   console.log(num1 + num2);
   //what if I have return here, or how do I have return here, since I dont have any variables ?
+  //What would I return ?
 }
 logNumbers(1, 1);
 
@@ -8,10 +9,11 @@ function multiplyNumbers(num3, num4) {
   let result;
   result = num3 * num4;
   console.log(result);
-  //return num3 * num4;
+  //return num3 * num4; =shorter version of this whole code ?
   return result;
 }
-multiplyNumbers(5, 5);
+let execution = multiplyNumbers(5, 5);
+console.log(execution);
 
 //why DOES THIS NOT WORK ? T.T
 //how do I execute console log outside the function
